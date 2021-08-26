0aa512eed36f6788eb8290d4de305da7180d6e8d
thebuntaro <thebuntaro@wp.pl>
2021-08-26 22:39:16 +0200

    add api,libraries,middleware,sites

--- main/index.ts
+++ main/index.ts
@@ -1,12 +1,37 @@
 import express from "express";
-import path  from "path";
+import sharp from "sharp";
+import fileUpload from "express-fileupload";
 const app = express();
 const port= process.env.PORT || 3000;
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"index.html"));
-});
+//view engine
+app.set("view engine","ejs");
+app.set("views","main/sites");
 
+//listen on port
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}...`)
 });
+
+//middleware
+app.use(express.static("public"));
+app.use(fileUpload({
+    //useTempFiles:true,
+    //tempFileDir:'main/files/'
+}));
+
+//api
+app.get("/",(req,res)=>{
+    res.render("index",{cssFile:"index.css"});
+});
+app.post("/upload",(req:any,res)=>{
+    //let imageFile=req.files.image_file;
+    //sharp(imageFile).resize(100).toFile("img100.png").then(r => console.log(r));
+
+    res.render("upload",{cssFile:"index.css"});
+});
+
+app.use((req,res)=>{
+    res.status(404).send("404 Not Found");
+});
+
