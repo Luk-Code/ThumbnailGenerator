9a4ea2eeb128a1016ccba80df9f502e63fc2a091
thebuntaro <thebuntaro@wp.pl>
2021-08-26 19:13:15 +0200

    configure typescript and add express

--- main/index.ts
+++ main/index.ts
@@ -1,1 +1,12 @@
-const app = require("http");
+import express from "express";
+import path  from "path";
+const app = express();
+const port= process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"index.html"));
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening on port ${port}...`)
+});
