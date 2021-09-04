19a6ee2779bcd695fccc07692846beb3688648c8
thebuntaro <thebuntaro@wp.pl>
2021-09-04 23:16:01 +0200

    Added responsiveness, add router, change styles, merge error sites to one file

--- main/index.ts
+++ main/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import path from "path";
-import sharp from "sharp";
-import fileUpload from "express-fileupload";
 import {v1 as uuidv1} from "uuid";
 import fs from "fs";
 const app = express();
 const port = process.env.PORT || 3000;
+const upload = require("./routers/upload");
 
 //view engine
 app.set("view engine","ejs");
@@ -20,13 +19,9 @@ app.listen(port, ()=>{
 app.use(express.static("public"));
 app.use('/upload', express.static("public"));
 app.use(express.urlencoded({extended: true}));
-app.use(fileUpload({
-    /*useTempFiles:true,
-    tempFileDir:'tmp/',
-    uploadTimeout:60,*/
-    createParentPath:true,
-    debug:false
-}));
+
+//update
+app.use('/upload',upload);
 
 //const
 const user_uuid:string=uuidv1().split('-').splice(1).join('-');
@@ -49,49 +44,7 @@ app.get("/",(req,res)=>{
     }
 });
 
-//upload
-app.post("/upload",async (req: any, res) => {
-    let imageFile: fileUpload.UploadedFile = req.files.image_file;
-    let imageFileName: string = imageFile.name;
-    let imageFileNameBase: string = imageFileName.split('.')[0];
-    let localImagePath:string = path.join(pathToUserImages, imageFileNameBase);
-    await imageFile.mv(path.join(localImagePath, imageFileName))
-        .then(() => console.log("upload success")).catch(err => console.log("upload error: ", err));
-    //change to png
-    await sharp(path.join(localImagePath, imageFileName))
-        .toFormat("png").png().toFile(path.join(localImagePath, "origin.png"))
-        .then(() => console.log("change to png success")).catch(err => console.log("change to png error: ", err));
-    //sharp 100
-    sharp(path.join(localImagePath, "origin.png")).resize(100, null)
-        .toFormat("png").png().toFile(path.join(localImagePath, "img100.png"))
-        .then(() => console.log("sharp 100 success")).catch(err => console.log("sharp 100 error: ", err));
-    //sharp 200
-    sharp(path.join(localImagePath, "origin.png")).resize(200, null)
-        .toFormat("png").png().toFile(path.join(localImagePath, "img200.png"))
-        .then(() => console.log("sharp 200 success")).catch(err => console.log("sharp 200 error: ", err));
-    //sharp 400
-    sharp(path.join(localImagePath, "origin.png")).resize(400, null)
-        .toFormat("png").png().toFile(path.join(localImagePath, "img400.png"))
-        .then(() => console.log("sharp 400 success")).catch(err => console.log("sharp 400 error: ", err));
-
-    console.log("redirect");
-    res.redirect(`/upload/${imageFileNameBase}`);
-});
-app.get("/upload/:fileName",(req,res)=>{
-    let imageFileName=req.params.fileName;
-    let pathToFile=path.join("images",user_uuid,imageFileName);
-    if(fs.existsSync(path.join("public",pathToFile))){
-        res.render("upload",{
-            cssFile:"upload.css",
-            path:pathToFile,
-            imageFileName
-        });
-    }else{
-        res.status(502).render("502",{cssFile:"index.css"});
-    }
-});
-
 app.use((req,res)=>{
-    res.status(404).render("404",{cssFile:"index.css"});
+    res.status(404).render("error",{cssFile:"error.css",error:404});
 });
 
---
+++ main/routers/upload.ts
@@ -0,0 +1,79 @@
+import fileUpload from "express-fileupload";
+import path from "path";
+import sharp from "sharp";
+import fs from "fs";
+import sizeOf from "image-size";
+import express from "express";
+import {v1 as uuidv1} from "uuid";
+const router = express.Router();
+
+const user_uuid:string=uuidv1().split('-').splice(1).join('-');
+const pathToUserImages:string=path.join("public","images",user_uuid);
+
+router.use(fileUpload({
+    /*useTempFiles:true,
+    tempFileDir:'tmp/',
+    uploadTimeout:60,*/
+    createParentPath:true,
+    debug:false
+}));
+
+router.post("",async (req: any, res) => {
+    if(!req.files) {
+        res.status(400).render("error",{
+            cssFile:"error.css",error:400,message:"400 No file uploaded"});
+        return;
+    }
+    let imageFile: fileUpload.UploadedFile = req.files.image_file;
+    if(req.files.image_file.mimetype!=="image/jpeg" && req.files.image_file.mimetype!=="image/png"){
+        res.status(400).render("error",{
+            cssFile:"error.css",error:400,message:"400 File isn't a image"});
+        return;
+    }
+    let imageFileName: string = imageFile.name;
+    let imageFileNameBase: string = imageFileName.split('.')[0];
+    let localImagePath: string = path.join(pathToUserImages, imageFileNameBase);
+    await imageFile.mv(path.join(localImagePath, imageFileName))
+        .then(() => console.log("upload success")).catch(err => console.log("upload error: ", err));
+    //change to png
+    await sharp(path.join(localImagePath, imageFileName))
+        .toFormat("png").png().toFile(path.join(localImagePath, "origin.png"))
+        .then(() => console.log("change to png success")).catch(err => console.log("change to png error: ", err));
+    //sharp 100
+    sharp(path.join(localImagePath, "origin.png")).resize(100, null)
+        .toFormat("png").png().toFile(path.join(localImagePath, "img100.png"))
+        .then(() => console.log("sharp 100 success")).catch(err => console.log("sharp 100 error: ", err));
+    //sharp 200
+    sharp(path.join(localImagePath, "origin.png")).resize(200, null)
+        .toFormat("png").png().toFile(path.join(localImagePath, "img200.png"))
+        .then(() => console.log("sharp 200 success")).catch(err => console.log("sharp 200 error: ", err));
+    //sharp 400
+    sharp(path.join(localImagePath, "origin.png")).resize(400, null)
+        .toFormat("png").png().toFile(path.join(localImagePath, "img400.png"))
+        .then(() => console.log("sharp 400 success")).catch(err => console.log("sharp 400 error: ", err));
+    console.log("redirect");
+    res.redirect(`/upload/${imageFileNameBase}`);
+});
+router.get("/:fileName",(req,res)=>{
+    let imageFileName=req.params.fileName;
+    let pathToFile=path.join("images",user_uuid,imageFileName);
+    if(fs.existsSync(path.join("public",pathToFile))){
+        let originWidth=sizeOf(path.join(pathToUserImages,imageFileName,"origin.png")).width as number;
+        let originHeight=sizeOf(path.join(pathToUserImages,imageFileName,"origin.png")).height as number;
+        let imgHeight={
+            img100:Math.round((100/originWidth)*originHeight),
+            img200:Math.round((200/originWidth)*originHeight),
+            img400:Math.round((400/originWidth)*originHeight)
+        }
+        res.render("upload",{
+            cssFile:"upload.css",
+            path:pathToFile,
+            imageFileName,
+            imgHeight
+        });
+    }else{
+        res.status(502).render("error",{cssFile:"error.css",error:502});
+    }
+});
+
+module.exports=router;
